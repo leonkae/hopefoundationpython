3094d3a688a4be239f6eada39dc79f9651399371
leonkae <leon29kae@gmail.com>
2025-01-23 18:16:19 +0300

    setup

--- hopeapp/views.py
+++ hopeapp/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView
 # Create your views here.
+
+class HomeView(ListView):
+    template_name = "hopeTemp/index.html"
+    
+    def get(self, request):
+        context ={
+            
+        }
+        
+        return render(request, self.template_name,context)

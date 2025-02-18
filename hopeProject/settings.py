8ac9cc2df865a66ca2fe23a81c66e14257ab23f4
leonkae <leon29kae@gmail.com>
2025-02-18 23:30:35 +0300

    modify:alot -db

--- hopeProject/settings.py
+++ hopeProject/settings.py
@@ -117,21 +117,21 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "/static/"
-# STATICFILES_DIRS = [
-#     BASE_DIR / 'static'
-# ]
-# STATIC_ROOT = BASE_DIR / "staticfiles"
-# STATICFILES_FINDERS = (
-#     "django.contrib.staticfiles.finders.FileSystemFinder",
-#     "django.contrib.staticfiles.finders.AppDirectoriesFinder",
-#     "compressor.finders.CompressorFinder",
-# )
-# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_FINDERS = (
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+    "compressor.finders.CompressorFinder",
+)
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
-# # Media Configurations
-# MEDIA_DIR = BASE_DIR / "media"
-# MEDIA_ROOT = MEDIA_DIR
-# MEDIA_URL = "/media/"
+# Media Configurations
+MEDIA_DIR = BASE_DIR / "media"
+MEDIA_ROOT = MEDIA_DIR
+MEDIA_URL = "/media/"
 
 
 # Default primary key field type
